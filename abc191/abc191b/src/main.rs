ddf2b4b52837e89ea04a34138a2b3a48d419ab0d
ka2kama <ka2kama@outlook.jp>
2024-12-10 19:54:54 +0900

    abc191

--- abc191/abc191b/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused, nonstandard_style)]
-
-use itertools::Itertools;
-use proconio::input;
-
-fn main() {
-    input! { N: usize, X: usize, A: [usize; N] };
-    println!("{}", A.into_iter().filter(|&x| x != X).join(" "));
-}
