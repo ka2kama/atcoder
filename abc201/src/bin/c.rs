30be7e5c6da8e775926888c27fac241e7f40e243
ka2kama <ka2kama@outlook.jp>
2024-12-11 13:28:20 +0900

    abc201 c

--- abc201/src/bin/c.rs
+++ abc201/src/bin/c.rs
@@ -1,16 +1,58 @@
 #![allow(unused, nonstandard_style)]
 
-use std::iter;
-
+use ascii::{AsciiChar, AsciiString};
 use itertools::Itertools;
+use maplit::hashmap;
 use num_traits::ToPrimitive;
+use permutohedron::factorial;
+use petgraph::visit::Walker;
 use proconio::marker::{Chars, Usize1};
 use proconio::{derive_readable, fastout, input};
+use std::collections::HashSet;
+use std::iter::Iterator;
+
+const TEN_THOUSAND_DIGITS: [[usize; 4]; 10000] = {
+    let mut i = 0;
+    let mut v: [[usize; 4]; 10000] = [[0, 0, 0, 0]; 10000];
+    while i < 10000 {
+        v[i] = to_digit_array(i);
+        i += 1;
+    }
+    v
+};
+
+const fn to_digit_array(mut num: usize) -> [usize; 4] {
+    let mut digits = [0, 0, 0, 0];
+
+    let mut i = 3;
+    loop {
+        digits[i] = num % 10;
+        if i == 0 {
+            break digits;
+        }
+        num /= 10;
+        i -= 1;
+    }
+}
 
 #[fastout]
 fn main() {
-    todo!();
-    input! { N: usize, A: [i64; N], }
-    let ans = "";
+    input! { S: AsciiString }
+    let S: Vec<AsciiChar> = S.into();
+
+    let mandatory = S
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &ch)| if ch == AsciiChar::o { Some(i) } else { None })
+        .collect_vec();
+
+    let ans = TEN_THOUSAND_DIGITS
+        .into_iter()
+        .filter(|digits| {
+            digits.iter().all(|&d| S[d] != AsciiChar::x)
+                && mandatory.iter().all(|d| digits.contains(d))
+        })
+        .count();
+
     println!("{}", ans);
 }
