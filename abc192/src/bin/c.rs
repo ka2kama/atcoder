4c96e46d04aeb17a8869df5df928e516d8322944
ka2kama <ka2kama@outlook.jp>
2024-12-12 22:30:15 +0900

    abc192 c

--- abc192/src/bin/c.rs
+++ abc192/src/bin/c.rs
@@ -1,11 +1,13 @@
 #![allow(unused, nonstandard_style)]
 
-use ascii::{AsciiChar, AsciiString, IntoAsciiString};
-use itertools::Itertools;
+use ascii::{AsciiChar, AsciiStr, AsciiString, IntoAsciiString, ToAsciiChar};
+use itertools::{fold, Itertools};
 use num_traits::ToPrimitive;
 use proconio::marker::{Chars, Usize1};
 use proconio::source::{Readable, Source};
 use proconio::{derive_readable, fastout, input};
+use smallvec::SmallVec;
+use std::cmp::Reverse;
 use std::io::BufRead;
 
 enum AsciiChars {}
@@ -20,23 +22,27 @@ impl Readable for AsciiChars {
 
 fn main() {
     input! {
-        N: usize,
-        A: AsciiChars,
+        N: u64, K: usize
     }
 
-    let app = App { N, A };
-    app.run();
+    let ans = (0..K).fold(N, |acc, _| f(acc));
+    println!("{}", ans);
 }
 
-struct App {
-    N: usize,
-    A: Vec<AsciiChar>,
-}
-
-impl App {
-    #[fastout]
-    fn run(self) {
-        let ans = "";
-        println!("{}", ans);
-    }
+fn f(mut n: u64) -> u64 {
+    let v = {
+        let mut v: SmallVec<[u64; 10]> = SmallVec::new();
+        while n > 0 {
+            v.push(n % 10);
+            n /= 10;
+        }
+        v.sort_unstable();
+        v
+    };
+    let asc = v.iter().rev().fold(0u64, |acc, &x| acc * 10 + x);
+    let desc = v
+        .iter()
+        .skip_while(|&&x| x == 0)
+        .fold(0u64, |acc, &x| acc * 10 + x);
+    asc - desc
 }
