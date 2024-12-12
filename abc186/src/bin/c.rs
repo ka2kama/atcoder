403239acf638fa1a6b09f065726347911f48331a
ka2kama <ka2kama@outlook.jp>
2024-12-12 23:33:05 +0900

    abc186 c

--- abc186/src/bin/c.rs
+++ abc186/src/bin/c.rs
@@ -1,39 +1,15 @@
 #![allow(unused, nonstandard_style)]
 
-use ascii::{AsciiChar, IntoAsciiString};
-use proconio::source::{Readable, Source};
-use proconio::{fastout, input};
-use std::io::BufRead;
-
-enum AsciiChars {}
-
-impl Readable for AsciiChars {
-    type Output = Vec<AsciiChar>;
-    fn read<R: BufRead, S: Source<R>>(source: &mut S) -> Vec<AsciiChar> {
-        let token = source.next_token_unwrap();
-        token.into_ascii_string().unwrap().into()
-    }
-}
+use proconio::input;
+use proconio::source::Readable;
 
 fn main() {
     input! {
         N: usize,
-        A: AsciiChars,
     }
 
-    let app = App { N, A };
-    app.run();
-}
-
-struct App {
-    N: usize,
-    A: Vec<AsciiChar>,
-}
-
-impl App {
-    #[fastout]
-    fn run(self) {
-        let ans = "";
-        println!("{}", ans);
-    }
+    let ans = (1..=N)
+        .filter(|&x| !format!("{}{:o}", x, x).contains('7'))
+        .count();
+    println!("{}", ans);
 }
