63bfd6320f90716b062723eaaa7e790ff9c238e8
ka2kama <ka2kama@outlook.jp>
2024-12-26 10:09:26 +0900

    update template

--- abc385/src/bin/c.rs
+++ abc385/src/bin/c.rs
@@ -1,169 +1,157 @@
 #![allow(unused, nonstandard_style)]
 
+use crate::my_lib::my_iter::*;
+use crate::my_lib::my_num::*;
 use crate::my_lib::*;
-use ascii::{AsciiChar, AsciiString};
-use indexing::container_traits::Pushable;
-use indexmap::indexmap;
 use itertools::Itertools;
 use maplit::{hashmap, hashset};
-use proconio::{derive_readable, input};
+use proconio::marker::{Chars, Usize1};
+use proconio::{derive_readable, fastout, input};
 use std::collections::*;
 use std::mem;
 
-mod my_lib {
-    use ascii::{AsciiChar, IntoAsciiString};
-    use proconio::source::{Readable, Source};
-    use std::io::BufRead;
-    use std::iter::Rev;
+pub mod my_lib {
+    pub mod my_iter {
+        use crate::my_lib::my_iter::scan_left::ScanLeft;
+        use std::iter::Rev;
+        pub trait MyIterator: Iterator {
+            #[inline]
+            fn scan_left<St, F>(self, initial_state: St, f: F) -> ScanLeft<Self, St, F>
+            where
+                Self: Sized,
+                St: Clone,
+                F: FnMut(&mut St, Self::Item),
+            {
+                ScanLeft::new(self, initial_state, f)
+            }
 
-    pub struct SimpleScan<I, St, F> {
-        iter: I,
-        state: St,
-        f: F,
-        started: bool,
-    }
-
-    impl<I, St, F> SimpleScan<I, St, F> {
-        fn new(iter: I, initial_state: St, f: F) -> SimpleScan<I, St, F> {
-            SimpleScan {
-                iter,
-                state: initial_state,
-                f,
-                started: false,
+            #[inline]
+            fn scan_right<St, F>(self, initial_state: St, f: F) -> ScanLeft<Rev<Self>, St, F>
+            where
+                Self: Sized + DoubleEndedIterator,
+                St: Clone,
+                F: FnMut(&mut St, Self::Item),
+            {
+                self.rev().scan_left(initial_state, f)
             }
         }
 
-        fn snap_shot(&self) -> St
-        where
-            St: Clone,
-        {
-            self.state.clone()
-        }
-    }
+        impl<I: Iterator> MyIterator for I {}
 
-    impl<I, St, F, A> Iterator for SimpleScan<I, St, F>
-    where
-        St: Clone,
-        I: Iterator<Item = A>,
-        F: FnMut(&mut St, A),
-    {
-        type Item = St;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            if !self.started {
-                self.started = true;
-                Some(self.snap_shot())
-            } else if let Some(next) = self.iter.next() {
-                (self.f)(&mut self.state, next);
-                Some(self.snap_shot())
-            } else {
-                None
+        mod scan_left {
+            #[derive(Clone)]
+            pub struct ScanLeft<I, St, F> {
+                iter: I,
+                state: St,
+                f: F,
+                started: bool,
             }
-        }
 
-        fn size_hint(&self) -> (usize, Option<usize>) {
-            (0, self.iter.size_hint().1)
-        }
-    }
-
-    pub trait SimpleScanIterator: Iterator {
-        #[inline]
-        fn scan_left<St, F>(self, initial_state: St, f: F) -> SimpleScan<Self, St, F>
-        where
-            Self: Sized,
-            F: FnMut(&mut St, Self::Item),
-        {
-            SimpleScan::new(self, initial_state, f)
-        }
-
-        #[inline]
-        fn scan_right<St, F>(self, initial_state: St, f: F) -> SimpleScan<Rev<Self>, St, F>
-        where
-            Self: Sized + DoubleEndedIterator,
-            F: FnMut(&mut St, Self::Item),
-        {
-            self.rev().scan_left(initial_state, f)
-        }
-    }
-
-    impl<I: Iterator> SimpleScanIterator for I {}
-
-    pub trait AsSize {
-        fn as_isize(&self) -> isize;
-        fn as_usize(&self) -> usize;
-    }
-
-    macro_rules! impl_as_size {
-        ( $t:ty ) => {
-            impl AsSize for $t {
-                fn as_isize(&self) -> isize {
-                    (*self).try_into().unwrap()
-                }
-
-                fn as_usize(&self) -> usize {
-                    (*self).try_into().unwrap()
+            impl<I, St, F> ScanLeft<I, St, F> {
+                #[inline]
+                pub(in my_iter) fn new(iter: I, state: St, f: F) -> Self {
+                    Self {
+                        iter,
+                        state,
+                        f,
+                        started: false,
+                    }
                 }
             }
-        };
+
+            impl<I, St, F> Iterator for ScanLeft<I, St, F>
+            where
+                St: Clone,
+                I: Iterator,
+                F: FnMut(&mut St, I::Item),
+            {
+                type Item = St;
+
+                fn next(&mut self) -> Option<St> {
+                    if !self.started {
+                        // 初回実行
+                        self.started = true;
+                    } else {
+                        // 2回目以降
+                        let a = self.iter.next()?;
+                        (self.f)(&mut self.state, a);
+                    }
+                    Some(self.state.clone())
+                }
+            }
+        }
     }
 
-    impl_as_size!(i8);
-    impl_as_size!(i16);
-    impl_as_size!(i32);
-    impl_as_size!(i64);
-    impl_as_size!(isize);
-    impl_as_size!(u8);
-    impl_as_size!(u16);
-    impl_as_size!(u32);
-    impl_as_size!(u64);
-    impl_as_size!(usize);
-
-    pub enum AsciiChars {}
-
-    impl Readable for AsciiChars {
-        type Output = Vec<AsciiChar>;
-        fn read<R: BufRead, S: Source<R>>(source: &mut S) -> Vec<AsciiChar> {
-            let token = source.next_token_unwrap();
-            token.into_ascii_string().unwrap().into()
+    pub mod my_num {
+        pub trait AsSize {
+            fn as_isize(&self) -> isize;
+            fn as_usize(&self) -> usize;
         }
+
+        macro_rules! impl_as_size {
+            ( $t:ty ) => {
+                impl AsSize for $t {
+                    fn as_isize(&self) -> isize {
+                        (*self).try_into().unwrap()
+                    }
+
+                    fn as_usize(&self) -> usize {
+                        (*self).try_into().unwrap()
+                    }
+                }
+            };
+        }
+
+        impl_as_size!(i8);
+        impl_as_size!(i16);
+        impl_as_size!(i32);
+        impl_as_size!(i64);
+        impl_as_size!(isize);
+        impl_as_size!(u8);
+        impl_as_size!(u16);
+        impl_as_size!(u32);
+        impl_as_size!(u64);
+        impl_as_size!(usize);
     }
 }
 
-struct Building {
-    pos: isize,
-    height: isize,
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct Pos {
+    x: usize,
+    y: usize,
 }
 
 #[cfg(target_pointer_width = "64")]
+#[fastout]
 fn main() {
     input! {
-        N: usize,
-        A: [isize; N]
+        H: isize, W: isize, X: Usize1, Y: Usize1,
+        A: [Chars; H],
+        T: Chars
     }
 
-    let mut im = indexmap! {};
-    A.into_iter().enumerate().for_each(|(i, h)| {
-        if !im.contains_key(&h) {
-            im.insert(h, vec![false; N]);
-        }
-        im.get_mut(&h).unwrap()[i] = true;
-    });
+    let mut houses = hashmap![];
+    let (mut x, mut y) = (X, Y);
+    for command in T {
+        let (next_x, next_y) = match command {
+            'U' => (x - 1, y),
+            'D' => (x + 1, y),
+            'L' => (x, y - 1),
+            'R' => (x, y + 1),
+            _ => unreachable!(),
+        };
 
-    let mut max = 0isize;
-    for (_, v) in im.iter() {
-        for interval in 1..N {
-            let mut cnt = 0;
-            let mut i = 0;
-            while i < N {
-                if v[i] && i + interval < N && v[i + interval] {
-                    cnt += 1;
-                    i += interval;
-                } else {
-                    i += 1;
+        match A[next_x][next_y] {
+            '#' => {}
+            ch => {
+                (x, y) = (next_x, next_y);
+                if ch == '@' {
+                    houses.insert((x, y), true);
                 }
             }
-            max = max.max(cnt);
         }
     }
-    println!("{}", max + 1);
+
+    let cnt = houses.values().filter(|&arrived| *arrived).count();
+    println!("{} {} {}", x + 1, y + 1, cnt);
 }
